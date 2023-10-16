1fd31784ec46ace140c3b886ebaada8cc6e5ba16
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-16 16:36:16 +0200

    Enable adding new products to database

---
+++ src/main/java/com/example/learningspringbootproducts/product/ProductController.java
@@ -0,0 +1,25 @@
+package com.example.learningspringbootproducts.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
+
+}
--- src/main/java/com/example/learningspringbootproducts/product/ProductService.java
+++ src/main/java/com/example/learningspringbootproducts/product/ProductService.java
@@ -3,9 +3,21 @@ package com.example.learningspringbootproducts.product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public Product addProduct(Product product) {
+        return productRepo.save(product);
+    }
+
+
 }
